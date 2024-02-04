3c968b3e945f2f02bebbe2ff6afbf3d047fd472a
Evandrolds <evandro.lds.ds@outlook.com>
2024-02-04 11:04:55 -0300

    criação do Projeto

---
+++ src/test/java/com/evandro/gerenciartarefas/GerenciarTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evandro.gerenciartarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
